58a4116c8319bded69cc39f122e1d7da61d9ac60
RPDeshaies <rpdeshaies@outlook.com>
2016-02-11 16:21:00 -0500

    Added a way to generate guids and updated the html

--- services/chaptr/chaptrController.js
+++ services/chaptr/chaptrController.js
@@ -4,8 +4,10 @@
     
     function chaptrController(dialogModel) {
         var vm = this;
-        vm.data = {
-            chapterName : "",
+        vm.chapter = {
+            id : "",
+            name : "",
+            language : "",
             dialogs : [],
         };
         
@@ -17,6 +19,14 @@
         }
         vm.exportData = function () {
 
+        }
+        
+        function generateGuid(){
+            var guid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                return v.toString(16);
+            });
+            return guid;
         }
     }
 })();
