4e3e87addbbd8d9323ed3b2cad75fbe0f4895a3a
René-Pier Deshaies-Gélinas <Ren-Pier Deshaies-Glinas>
2016-02-10 23:03:57 -0500

    Added a basic api to save a json file in the server

--- server.js
+++ server.js
@@ -13,4 +13,16 @@ app.get('/', function (req, res) {
     res.render('pages/index');
 });
 
+app.get('/api/text/{chapterName}', function (req, res) {
+
+});
+
+app.post('/api/text/:chapterName', function (req, res) {
+    var fs = require('fs');
+    fs.writeFile("/databases/" + req.params.chapterName + ".json", req.body, function (err) {
+        if (err) {
+        }
+    });
+});
+
 console.log("Server running");
