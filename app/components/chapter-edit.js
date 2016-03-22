fc84c2457b1c2858b059690f6623d4321edfc2c8
RPDeshaies <rpdeshaies@outlook.com>
2016-03-22 11:34:40 -0400

    Added basic actions in the chapter edit route

--- app/components/chapter-edit.js
+++ app/components/chapter-edit.js
@@ -1,8 +1,19 @@
 import Ember from 'ember';
+import Repo from "ay-ember/utils/repo";
 
 export default Ember.Component.extend({
-    dialog : {},
+    store: Ember.inject.service(),
+    chapter : {},
     actions : {
-        
+        saveEverything(){
+            var store = this.get("store");
+            var chapter = this.get("chapter");
+            chapter.id = chapter.id || Repo.guid();
+            
+            localStorage.chapter = JSON.stringify(vm.chapterData.chapter);
+        },
+        clearEverything(){
+            
+        }
     }
 });
--- app/models/chapter.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  id: DS.attr(),
-  name: DS.attr(),
-  number: DS.attr(),
-  language: DS.attr(),
-  dialogs: DS.attr(),
-});
